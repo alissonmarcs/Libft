f0ad1f0ba23c29bb24366e74d8a89c62cdfa5fd4
Alisson Marcos Serafim De Souza <almarcos@c3r4p5.42sp.org.br>
2023-08-02 10:11:38 -0300

    memset implementation

--- ft_memset.c
+++ ft_memset.c
@@ -1,1 +1,26 @@
-void *memset(void s[.n], int c, size_t n);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almarcos <almarcos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 10:18:59 by almarcos          #+#    #+#             */
+/*   Updated: 2023/07/22 11:17:27 by almarcos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*current_byte;
+
+	current_byte = (unsigned char *)s;
+	while (current_byte < (unsigned char *)(s + n))
+	{
+		*current_byte = c;
+		current_byte++;
+	}
+	return (s);
+}
