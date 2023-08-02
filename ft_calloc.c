6289a86267aeee14ba0980c26081023df38a5e6c
Alisson Marcos Serafim De Souza <almarcos@c3r4p5.42sp.org.br>
2023-08-02 12:06:41 -0300

    fixing errors

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,31 +6,16 @@
 /*   By: almarcos <almarcos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 16:07:55 by almarcos          #+#    #+#             */
-/*   Updated: 2023/08/02 11:34:41 by almarcos         ###   ########.fr       */
+/*   Updated: 2023/08/02 12:05:38 by almarcos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	*ft_calloc(size_t nmemb, size_t size)
-// {
-// 	void	*ptr;
-
-// 	if ((!nmemb || !size))
-// 		return (malloc(0));
-// 	if (size > (SIZE_MAX / nmemb))
-// 		return (NULL);
-// 	ptr = malloc(nmemb * size);
-// 	if (ptr == NULL)
-// 		return (NULL);
-// 	ft_bzero(ptr, nmemb * size);
-// 	return (ptr);
-// }
-
-void *ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-	void *ptr;
-	size_t n_bytes;
+	void	*ptr;
+	size_t	n_bytes;
 
 	if (!nmemb || !size)
 		return (malloc(0));
@@ -43,11 +28,3 @@ void *ft_calloc(size_t nmemb, size_t size)
 	ft_bzero(ptr, n_bytes);
 	return (ptr);
 }
-
-// int main()
-// {
-// 	char *ptr = calloc(8, 1);
-// 	ft_strlcpy(ptr, "alisson\0", 10);
-// 	printf("%s\n", ptr);
-// 	free(ptr);
-// }
