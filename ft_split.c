bc56e38b0d3beeff4c588f513b92b462a1a99ac1
Alisson Marcos Serafim De Souza <almarcos@c1r10p2.42sp.org.br>
2023-08-04 14:05:14 -0300

    split implementation

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,11 @@
 /*   By: almarcos <almarcos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 14:22:35 by almarcos          #+#    #+#             */
-/*   Updated: 2023/08/04 09:53:42 by almarcos         ###   ########.fr       */
+/*   Updated: 2023/08/04 14:04:19 by almarcos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <malloc.h>
 
 static size_t	ft_word_count(const char *s, int c)
 {
@@ -20,12 +19,14 @@ static size_t	ft_word_count(const char *s, int c)
 	words = 0;
 	while (*s)
 	{
-		if (*s != (unsigned char)c && *s)
+		if (*s != c)
 		{
 			words++;
-			while (*s != (unsigned char)c && *s)
+			while (*s != c && *s)
 				s++;
 		}
+		if (*s == '\0')
+			return (words);
 		s++;
 	}
 	return (words);
@@ -40,38 +41,42 @@ static char	**ft_alloc_words(char **array, const char *s, int c)
 	while (*s)
 	{
 		current_word_len = 0;
-		while (s[current_word_len] != (unsigned char)c && s[current_word_len])
+		while (s[current_word_len] != c && s[current_word_len])
 			current_word_len++;
-		array[index] = ft_calloc(current_word_len + 1, sizeof(char));
+		array[index] = (char *)ft_calloc((current_word_len + 1), sizeof(char));
 		ft_strlcpy(array[index], s, current_word_len + 1);
 		index++;
-		while (*s != (unsigned char)c && *s)
+		while (*s != c && *s)
 			s++;
-		while (*s == (unsigned char)c && *s)
+		while (*s == c && *s)
 			s++;
 	}
 	array[index] = NULL;
 	return (array);
 }
 
-// static void	*free_words(char **array)
-// {
-// 	size_t	i;
+static void	*ft_free_words(char **array)
+{
+	size_t	i;
 
-// 	i = 0;
-// 	while (array[i])
-// 		free(array[i++]);
-// 	free(array);
-// 	return (NULL);
-// }
+	i = 0;
+	while (array[i])
+		free(array[i++]);
+	free(array);
+	return (NULL);
+}
 
 char	**ft_split(char const *s, char c)
 {
 	char	**array;
 	size_t	words;
 
+	if (!s)
+		return (NULL);
 	words = ft_word_count(s, c);
-	array = ft_calloc(words + 1, sizeof(char *));
+	array = (char **)ft_calloc((words + 1), sizeof(char *));
+	if (!array)
+		return (NULL);
 	while (*s == (unsigned char)c && *s)
 		s++;
 	ft_alloc_words(array, s, c);
