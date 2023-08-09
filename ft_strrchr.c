8c9078fb122bf86739e0d200bbfd4282cb840285
Alisson Marcos Serafim De Souza <almarcos@c1r10p2.42sp.org.br>
2023-08-09 15:44:45 -0300

    when s[0] occour more than one in s and when c occour before others occurrences of s[0], we have error. solving this.

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: almarcos <almarcos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 10:29:50 by almarcos          #+#    #+#             */
-/*   Updated: 2023/07/24 12:45:15 by almarcos         ###   ########.fr       */
+/*   Updated: 2023/08/09 15:43:53 by almarcos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_strrchr(const char *s, int c)
 
 	len = ft_strlen(s);
 	end = (char *)(s + len);
-	while (*end != *s && *end != (unsigned char)c)
+	while (end != s && *end != (unsigned char)c)
 		end--;
 	if (*end == (unsigned char)c)
 		return (end);
-	return (0);
+	return (NULL);
 }
